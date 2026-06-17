76e235ebf69f0fd652dc5800ccbdea0e7fb100a2
Maha <maha.is.available@gmail.com>
2026-06-17 19:27:12 +0530

    Head First Ch1.1

---
+++ src/basics/CodeMagnetsSfuffel.java
@@ -0,0 +1,27 @@
+package basics;
+
+public class CodeMagnetsSfuffel {
+	public static void main(String args[]) {
+		
+		int x=3;
+		while(x>0) {
+			if(x>2) {
+				System.out.print("a");
+			}
+			x-=1;
+			System.out.print("-");
+			if(x==1) {
+				System.out.print("d");
+				x-=x;
+			}
+			if(x==2) {
+				System.out.print("b c");
+			}
+			
+			
+		}
+			
+		
+	}
+
+}
---
+++ src/basics/PoolPuzzleOne.java
@@ -0,0 +1,28 @@
+package basics;
+
+public class PoolPuzzleOne {
+	public static void main(String args[]) {
+		int x = 0;
+		while(x<4) {
+			System.out.print("a");
+			if(x<1) {
+				System.out.print(" ");
+			}
+			System.out.print("n");
+			if(x<1) {
+				System.out.print("oise");
+				x-=1;
+			}
+			if(x==1) {
+				System.out.print("noys");
+			}
+			if(x>1) {
+				System.out.print(" oyster");
+			}
+			System.out.println();
+			x+=2;
+		}
+	}
+	
+
+}
