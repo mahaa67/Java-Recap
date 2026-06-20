60c1f553c12ee2fb72d5a5ea5eea9cce146103cf
Maha <maha.is.available@gmail.com>
2026-06-20 17:57:27 +0530

    20-06-2026 Update

---
+++ src/classesandobj/DrumKit.java
@@ -0,0 +1,15 @@
+package classesandobj;
+
+class DrumKit {
+	boolean topHat = true;
+	boolean snare=true;
+	void playSnare(){
+		System.out.println("bang bang ba-bang");
+		
+	}
+	void playTopHat() {
+		System.out.println("ding ding da-ding");
+		
+	}
+}
+
---
+++ src/classesandobj/Echo.java
@@ -0,0 +1,9 @@
+package classesandobj;
+
+class Echo {
+	int count = 0;
+	void hello() {
+		System.out.println("helloooo...");
+	}
+
+}
